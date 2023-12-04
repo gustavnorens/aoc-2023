a51176e0e7d4b0587b2714d516a246a73f764100
Speer <gustav01.noren@gmail.com>
2023-12-04 13:02:07 +0100

    cleaned 4

--- solutions/problem4.py
+++ solutions/problem4.py
@@ -1,12 +1,12 @@
 import uf
 import time
 start_time = time.time()
-input = uf.read_lines("../in/input4.txt")[:-1]
-test = uf.read_lines("../test/test4.txt")[:-1]
-def p1(lines):
-    res = map(score, lines)
-    return sum(res)
 
+input = uf.read_lines("../in/input4.txt")[:-1]
+
+def p1(lines):
+    res,_ = zip(*map(score, lines))
+    return sum(res)
 
 def score(game):
     game = game[10:]
@@ -16,35 +16,19 @@ def score(game):
     total = 0
     for s in scores:
         if s in wins:
-            if total == 0:
-                total += 1
-            else:
-                total *= 2
-    return total
-
-def score2(game):
-    game = game[10:]
-    wins, scores = game.split("|")
-    wins = wins.split()
-    scores = scores.split()
-    total = 0
-    for s in scores:
-        if s in wins:
             total += 1
-    return total
+    return int(2**(total-1)), total
 
-def countGames(cards):
+def p2(cards):
     R = len(cards)
     dick = {key: 1 for key in range(0,R)}
     for r, card in enumerate(cards):
-        res = score2(card)
+        res = score(card)[1]
         for i in range(1, res+1):
             dick[r+i] += dick[r]
     return sum(value for key, value in dick.items() if key <= R)
 
-
-
 print(p1(input))
-print(countGames(input))
+print(p2(input))
 
 print("--- %s seconds ---" % (time.time() - start_time))
