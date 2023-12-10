43c488fb1dcf294d4bc82a402293b741863d64df
Speer <gustav01.noren@gmail.com>
2023-12-10 18:47:00 +0100

    cleaned

--- solutions/problem10.py
+++ solutions/problem10.py
@@ -15,78 +15,50 @@ dick = {
 }
 
 
-lines = uf.read_lines("../in/10.in")[:-1]
+lines = uf.read_lines("../in/d10.txt")[:-1]
 tests = uf.read_lines("../test/10.in")[:-1]
 
-class Graph: #Chat gpt's graf
-    def __init__(self):
-        self.vertices = {}
-
-    def add_vertex(self, vertex):
-        if vertex not in self.vertices:
-            self.vertices[vertex] = []
-
-    def add_edge(self, vertex1, vertex2):
-        if vertex1 in self.vertices and vertex2 in self.vertices:
-            self.vertices[vertex1].append(vertex2)
-
-    def display_graph(self):
-        for vertex, neighbors in self.vertices.items():
-            print(f"{vertex}: {neighbors}")
-
-g = Graph()
-
-def connection(c1, c2):
-    l1 = dick[c1]
-    l2 = dick[c2]
-    for item in l1:
-        if item in l2:
-            return True
-    return False
+vertices = []
 
 def count_changes(j, line, r):
     t = 0
     for i in range(0, j):
-        if line[i] in "JL|S" and (r,i) in g.vertices.keys():
+        if line[i] in "JL|" and (r,i) in vertices:
             t += 1
     if t % 2 == 1:
         return 1
     return 0
 
 def findLoop(matrix):
-    arr = [list(row) for row in matrix]
-    R = len(matrix)
-    C = len(matrix[0])
-    s1 = 0
-    s2 = 0
+    R,C = len(matrix), len(matrix[0])
+    s1, s2 = 0, 0
     for i, line in enumerate(matrix):
         for j, c in enumerate(line):
             if c == "S":
-                s1 = i
-                s2 = j
-    c1 = s1
-    c2 = s2
-    
+                s1,s2 = i,j
+    c1, c2 = s1, s2
     while True:
-        if c1+1 < R and "north" in dick[matrix[c1+1][c2]] and "south" in dick[matrix[c1][c2]] and (not (c1+1,c2) in g.vertices.keys()):
+        if c1+1 < R and "north" in dick[matrix[c1+1][c2]] and "south" in dick[matrix[c1][c2]] and (not (c1+1,c2) in vertices):
             c1 += 1
-        elif c1-1 >= 0 and "south" in dick[matrix[c1-1][c2]] and "north" in dick[matrix[c1][c2]] and (not (c1-1,c2) in g.vertices.keys()):
+        elif c1-1 >= 0 and "south" in dick[matrix[c1-1][c2]] and "north" in dick[matrix[c1][c2]] and (not (c1-1,c2) in vertices):
             c1 -= 1
-        elif c2+1 < C and "west" in dick[matrix[c1][c2+1]] and "east" in dick[matrix[c1][c2]] and (not (c1,c2+1) in g.vertices.keys()):
+        elif c2+1 < C and "west" in dick[matrix[c1][c2+1]] and "east" in dick[matrix[c1][c2]] and (not (c1,c2+1) in vertices):
             c2 += 1
-        elif c2-1 >= 0 and "east" in dick[matrix[c1][c2-1]] and "west" in dick[matrix[c1][c2]] and (not (c1,c2-1) in g.vertices.keys()):
+        elif c2-1 >= 0 and "east" in dick[matrix[c1][c2-1]] and "west" in dick[matrix[c1][c2]] and (not (c1,c2-1) in vertices):
             c2 -= 1
-        g.add_vertex((c1,c2))
+        vertices.append((c1,c2))
         if (c1 == s1 and c2 == s2):
             break
-
+    f1, f2 = list(vertices)[0]
+    l1, l2 = list(vertices)[-1]
+    
     total = 0
     for i, line in enumerate(matrix):
         for j, c in enumerate(line):
-            if not (i,j) in g.vertices.keys():
+            if not (i,j) in vertices:
                 total += count_changes(j,line,i)
-    return len(g.vertices.keys()) // 2, total
-        
+    return len(vertices) // 2, total
+  
 res, total = findLoop(lines)
 
 print(res, total)
